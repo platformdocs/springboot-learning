89c81b987605379b7bd42d285711e85330254dd5
wuxiaobin <wuxiaobin@migu.cn>
2024-03-11 19:01:13 +0800

    update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,15 @@
 package com.example.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = "com.example")
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @SpringBootApplication()
+@ComponentScan(basePackages = {"com.example"})
+@MapperScan({"com.example.dao"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/service/UserService.java
+++ src/main/java/com/example/service/UserService.java
@@ -3,6 +3,7 @@ package com.example.service;
 import java.util.List;
 
 import com.example.model.User;
+import org.springframework.stereotype.Service;
 
 public interface UserService {
     List<User> getAllUsers();
